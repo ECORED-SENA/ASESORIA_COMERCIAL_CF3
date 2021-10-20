ed4a0d934d0952eea247168cb20f7e29cd7a8d93
Luis Urueta <lgurueta@misena.edu.co>
2021-10-19 19:15:26 -0500

    Avance01

---
+++ src/config/titulo.js
@@ -0,0 +1,1 @@
+module.exports = 'Manejo de la información'
---
+++ src/views/Introduccion.vue
@@ -0,0 +1,32 @@
+<template lang="pug">
+.curso-main-container.introduccion
+  BannerInterno
+  .container.tarjeta.tarjeta--blanca.p-4.p-md-5.mb-5
+  
+    .titulo-principal
+      .titulo-principal__numero
+        span
+          i.fas.fa-info
+      h1 Introducción
+    
+    figure.mb-5
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+    
+    .cajon-b.color-primario.p-3.mb-0
+      p.mb-0 Para que la empresa tome decisiones de mercado, debe tener claro a quienes va a servir, cuáles son sus características y necesidades, para ello es fundamental el manejo expedito de la información. En el desarrollo de este componente se trata aspectos del manejo de la información, la interpretación basada en los diferentes tipos de análisis cuantitativos o cualitativos, ello define la propuesta de la empresa a cada uno de los segmentos de mercado seleccionados. Así mismo, se realiza un recorrido por la recolección de la información y sus diferentes técnicas de recogida, operacionalización de variables, además de los pasos para definir el procesamiento de la información, preparación, interpretación y almacenamiento. Luego se mira qué estrategias de análisis de datos tradicionales y modernos recurren las empresas de hoy para alcanzar una mayor cobertura de mercado. Finalmente, los tipos de informes que se construyen con el proceso anteriormente expuesto y sus elementos básicos.
+
+</template>
+
+<script>
+export default {
+  name: 'Introduccion',
+  data: () => ({
+    // variables de vue
+  }),
+}
+</script>
+
+<style lang="sass" scoped>
+.introduccion
+</style>
